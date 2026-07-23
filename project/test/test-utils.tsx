5a7614f929821753950e2f8053722af0e605e764
App-netizen-arch <307964614+App-netizen-arch@users.noreply.github.com>
2026-07-23 15:42:21 +0500

    video-addition

--- project/test/test-utils.tsx
+++
@@ -1,7 +0,0 @@
-import type { ReactElement } from 'react';
-import { render, type RenderOptions } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-
-export function renderWithRouter(ui: ReactElement, options?: RenderOptions) {
-  return render(ui, { wrapper: MemoryRouter, ...options });
-}
